03b5e85593bb70eab016983b9f0cee449249e1a0
HyeJu <hae7077@naver.com>
2021-01-22 17:07:38 +0900

    add siren tab function

--- app/src/main/java/smu/techtown/withyou/Fragment/sirenFragment.java
+++ app/src/main/java/smu/techtown/withyou/Fragment/sirenFragment.java
@@ -1,21 +1,28 @@
 package smu.techtown.withyou.Fragment;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import smu.techtown.withyou.R;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.IOException;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class sirenFragment extends Fragment {
-
+    Button sirenStart;
+    Button sirenStop;
+    MediaPlayer mediaPlayer;
 
     public sirenFragment() {
         // Required empty public constructor
@@ -26,7 +33,28 @@ public class sirenFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_siren, container, false);
+        View view = inflater.inflate(R.layout.fragment_siren, container, false);
+        sirenStart = view.findViewById(R.id.sirenStart);
+        sirenStop = view.findViewById(R.id.sirenStop);
+        mediaPlayer = MediaPlayer.create(getActivity(),R.raw.policesiren);
+
+        sirenStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+
+        sirenStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                try{
+                    mediaPlayer.prepare();
+                }catch (IOException io){}
+            }
+        });
+        return view;
     }
 
 }
