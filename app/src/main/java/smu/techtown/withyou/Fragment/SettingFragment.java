746c014bb56a9bd5a065ccadce8e825020b1720e
HyeJu <hae7077@naver.com>
2021-02-03 16:11:24 +0900

    add function to set emergency number

--- app/src/main/java/smu/techtown/withyou/Fragment/SettingFragment.java
+++ app/src/main/java/smu/techtown/withyou/Fragment/SettingFragment.java
@@ -7,7 +7,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import smu.techtown.withyou.R;
 
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +20,7 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class SettingFragment extends Fragment {
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -25,11 +29,38 @@ public class SettingFragment extends Fragment {
                 .commit();
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment {
+    public static class MyPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        public static final String editTextKey = "phoneNumber";
+
+        public static EditTextPreference phoneNumEditText;
+        private PreferenceScreen screen;
+
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            screen = getPreferenceScreen();
+            phoneNumEditText = (EditTextPreference)screen.findPreference(editTextKey);
+            phoneNumEditText.setOnPreferenceChangeListener(this);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String value = (String)newValue;
+            if(preference == phoneNumEditText)
+                phoneNumEditText.setSummary(value);
+
+            return true;
+        }
+
+        public void onResume(){
+            super.onResume();
+            updateSummary();
+        }
+
+        public void updateSummary(){
+            phoneNumEditText.setSummary(phoneNumEditText.getText());
         }
     }
 
