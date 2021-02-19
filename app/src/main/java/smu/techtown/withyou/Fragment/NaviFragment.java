ca45aa4b6e7d5e4744a67aa4da379aeb0fa0a10e
HyeJu <hae7077@naver.com>
2021-02-19 16:26:07 +0900

    add code for connect url scheme and find current location

--- app/src/main/java/smu/techtown/withyou/Fragment/NaviFragment.java
+++ app/src/main/java/smu/techtown/withyou/Fragment/NaviFragment.java
@@ -1,32 +1,89 @@
 package smu.techtown.withyou.Fragment;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import smu.techtown.withyou.PreferenceManager;
 import smu.techtown.withyou.R;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapPoint;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NaviFragment extends Fragment {
-
-
-    public NaviFragment() {
-        // Required empty public constructor
-    }
-
+    LocationManager locationManager;
+    Location location;
+    double latitude;
+    double longitude;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_navi, container, false);
+        View view = inflater.inflate(R.layout.fragment_navi, container, false);
+        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    1);
+        }
+        else{
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if(location != null){
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+            }
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000,1,
+                    locationListener);
+        }
+
+        String currentLatitude = Double.toString(latitude);
+        String currentLongitude = Double.toString(longitude);
+        String url = "kakaomap://route?sp="+currentLatitude+","+currentLongitude+"&ep=37.4979502,127.0276368&by=FOOT";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+
+        return  view;
     }
+
+    final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    };
 
 }
